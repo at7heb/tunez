42518402955e3fce48a7255b43ee5331fac073bf
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2025-07-09 20:21:12 -0400

    json_api for Users

--- lib/tunez_web/ash_json_api_router.ex
+++ lib/tunez_web/ash_json_api_router.ex
@@ -1,6 +1,6 @@
 defmodule TunezWeb.AshJsonApiRouter do
   use AshJsonApi.Router,
-    domains: [Tunez.Music],
+    domains: [Tunez.Music, Tunez.Accounts],
     open_api: "/open_api",
     open_api_title: "TuneZ Music API Documentation",
     open_api_version: Application.spec(:tunez, :vsn) |> to_string()
