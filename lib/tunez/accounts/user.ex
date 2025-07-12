b88dcff58b9d5a628ba592818e6c5a6a87d39547
Howard Bussey <howard.bussey+gitmbp16@gmail.com>
2025-07-12 13:46:34 -0400

    add json authentication

--- lib/tunez/accounts/user.ex
+++ lib/tunez/accounts/user.ex
@@ -278,6 +278,16 @@ defmodule Tunez.Accounts.User do
     end
   end
 
+  policies do
+    bypass AshAuthentication.Checks.AshAuthenticationInteraction do
+      authorize_if always()
+    end
+
+    policy action([:register_with_password, :sign_in_with_password]) do
+      authorize_if always()
+    end
+  end
+
   attributes do
     uuid_primary_key :id
 
